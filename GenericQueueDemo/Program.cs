778b76fac006ab85f9d49606dde69a6966572c77
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 22:58:39 +0530

    GenericQueue

---
+++ GenericQueueDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericQueueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Queue<string> student = new Queue<string>();
+            student.Enqueue("Pankaj");
+            student.Enqueue("Pawan");
+            student.Enqueue("sujit");
+            student.Enqueue("vishal");
+            student.Enqueue("pandit");
+            print(student);
+            Console.WriteLine("\nRemoving {0} from List. \nNew list is : ", student.Dequeue());
+            print(student);
+            string[] city = { "pune", "Aurangabad", "Nashik" };
+            Queue<string> citylist = new Queue<string>(city);
+            Console.WriteLine("\nPrinting City List");
+            print(citylist);
+            Console.WriteLine("Count{0}", citylist.Count);
+        }
+        public static void print(Queue<string> q)
+        {
+            foreach (string s in q)
+            {
+                Console.Write(s.ToString() + " ");
+            }
+        }
+    }
+}
